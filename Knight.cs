e59007d7aeb2496a3e01447953ab374f74c67601
Ephanon <codeparadoxist@gmail.com>
2025-02-05 16:46:38 +0900

    using문 삭제

--- Knight.cs
+++ Knight.cs
@@ -5,8 +5,7 @@ using System.Net.Security;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+
 namespace TextRPG
 {
     public partial class Knight
