51da39965ceb40885b557d370e932572950805b3
Ephanon <codeparadoxist@gmail.com>
2025-02-05 22:01:59 +0900

    상태보기 실수 짚어보기

--- Knight_PrintInfo.cs
+++ Knight_PrintInfo.cs
@@ -16,7 +16,7 @@ namespace TextRPG
             while (true)
             {
 
-                if (inform)
+                if (inform) // 잘 못 입력했을 때 반복적으로 정보 보여주는걸 막기 위해 조건문을 달음
                 {
                     Console.Clear();
                     Console.WriteLine();
@@ -35,40 +35,46 @@ namespace TextRPG
                     for (int i = 0; i < equipment.Length; i++) //  장비 합산
                     {
 
+                        /* 상태보기에서 제일 문제라고 생각되는 부분..리스트는 타입이 다른 값들이 들어갈 수 있으니  아이템을 리스트로 선언해서 방어력끼리 더하고 공격력끼리 더했어야했는데
+
+                                                굳이 stat배열을 선언해서 string으로 공격력이 들어오면 공격력을 더하고
+
+                                             방어력이 들어오면 방어력을 더하는 코드를 작성한게 진짜 큰 실수라고 생각 든다.*/
+
                         string[] stat = information[i].Split(" ");
-                        if (stat[0] == "공격력" && status[i] == true)
+                        if (stat[0] == "공격력" && status[i] == true) // 정보배열에 공격력이면 공격력 상승
                         {
                             totalAtk += int.Parse(stat[1]);
 
 
 
                         }
-                        else if (stat[0] == "방어력" && status[i] == true)
+                        else if (stat[0] == "방어력" && status[i] == true) // 정보배열에 방어력이면 방어력 상승
                         {
                             totalDef += int.Parse(stat[1]);
                         }
 
                     }
-                    if (totalAtk == 0)
+                    if (totalAtk == 0) // 공격력 장비 착용 안했을 때
                     {
-                        Console.WriteLine($"공격력 : {atkEquip}");
+                        Console.WriteLine($"공격력 : {atkEquip}"); // 공격력만
                     }
-                    else
+                    else // 장비 장비 착용 했을때
                     {
                         Console.WriteLine($"공격력 : {atkEquip} +({totalAtk})");
                     }
-                    if (totalDef == 0) // 스탯 추가 x
+                    if (totalDef == 0)  // 방어력 장비 착용 안했을 때
                     {
                         Console.WriteLine($"방어력 : {defEquip}");
                     }
-                    else
+                    else // 방어력 장비 착용 했을때
                     {
                         Console.WriteLine($"방어력 : {defEquip} +({totalDef})");
                     }
                     Console.WriteLine($"체력 : {hp}");
                     Console.WriteLine($"Gold : {gold} G");
 
-                    inform = false;
+                    inform = false; // 나가기 이외의 다른 값을 눌렀을 때 상태보기 반복을 방지하기 위한 불 값
                 }
 
                 Console.WriteLine();
